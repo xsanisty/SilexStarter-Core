46db6b2ed625d675c031d6edac68497e32ea2506
Ikhsan Agustian <ikhsan017@gmail.com>
2015-06-02 20:45:04 +0700

    update dependency to symfony 2.6

--- src/Console/Command/MigrationCommand.php
+++ src/Console/Command/MigrationCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SilexStarterInitCommand extends Command
+class MigrationCommand extends Command
 {
     protected function configure()
     {

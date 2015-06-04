d3529cfe557d9fec4b138000c870c628833b21b6
Ikhsan Agustian <ikhsan017@gmail.com>
2015-06-04 15:28:58 +0700

    update Request proxy

--- src/StaticProxy/Request.php
+++ src/StaticProxy/Request.php
@@ -15,4 +15,32 @@ class Request extends StaticProxy
     {
         return static::$container->get('request')-> isXmlHttpRequest();
     }
+
+    public static function get($key = null)
+    {
+        return ($key)
+                ? static::$container->get('request')->request->get($key)
+                : static::$container->get('request')->request->all();
+    }
+
+    public static function query($key = null)
+    {
+        return ($key)
+                ? static::$container->get('request')->query->get($key)
+                : static::$container->get('request')->query->all();
+    }
+
+    public static function cookie($key = null)
+    {
+        return ($key)
+                ? static::$container->get('request')->cookies->get($key)
+                : static::$container->get('request')->cookies->all();
+    }
+
+    public static function file($key = null)
+    {
+        return ($key)
+                ? static::$container->get('request')->files->get($key)
+                : static::$container->get('request')->files->all();
+    }
 }

db3431648bc2f3419595372120b486fe63ccb383
Ikhsan Agustian <ikhsan017@gmail.com>
2015-08-29 06:41:22 +0700

    update static proxy to request helper object

--- src/StaticProxy/Request.php
+++ src/StaticProxy/Request.php
@@ -11,50 +11,55 @@ class Request extends StaticProxy
         return 'request';
     }
 
+    protected static function getRequest()
+    {
+        return static::$container->get('request_stack')->getCurrentRequest();
+    }
+
     public static function ajax()
     {
-        return static::$container->get('request')-> isXmlHttpRequest();
+        return static::getRequest()->isXmlHttpRequest();
     }
 
     public static function get($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->request->get($key)
-                : static::$container->get('request')->request->all();
+                ? static::getRequest()->request->get($key)
+                : static::getRequest()->request->all();
     }
 
     public static function query($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->query->get($key)
-                : static::$container->get('request')->query->all();
+                ? static::getRequest()->query->get($key)
+                : static::getRequest()->query->all();
     }
 
     public static function cookie($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->cookies->get($key)
-                : static::$container->get('request')->cookies->all();
+                ? static::getRequest()->cookies->get($key)
+                : static::getRequest()->cookies->all();
     }
 
     public static function file($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->files->get($key)
-                : static::$container->get('request')->files->all();
+                ? static::getRequest()->files->get($key)
+                : static::getRequest()->files->all();
     }
 
     public static function server($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->server->get($key)
-                : static::$container->get('request')->server->all();
+                ? static::getRequest()->server->get($key)
+                : static::getRequest()->server->all();
     }
 
     public static function header($key = null)
     {
         return ($key)
-                ? static::$container->get('request')->headers->get($key)
-                : static::$container->get('request')->headers->all();
+                ? static::getRequest()->headers->get($key)
+                : static::getRequest()->headers->all();
     }
 }

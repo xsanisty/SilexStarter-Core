bcd340d677f761324744a62e8f9f058e37310d7b
Ikhsan Agustian <ikhsan017@gmail.com>
2015-09-28 13:30:16 +0700

    implement migration, start from the service provider

--- src/Provider/MigrationServiceProvider.php
+++ src/Provider/MigrationServiceProvider.php
@@ -4,19 +4,36 @@ namespace SilexStarter\Provider;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use SilexStarter\SilexStarter;
+use SilexStarter\Storage\JsonStorage;
 use SilexStarter\Migration\Migrator;
+use SilexStarter\Migration\MigrationRepository;
 
 class MigrationServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['migrator'] = $app->share(
-            function () {
-                return new Migrator();
+        $app['storage.json'] = $app->share(
+            function (Application $app) {
+                return new JsonStorage($app['filesystem'], ['path' => $app['path.app'] . 'storage/meta/']);
             }
         );
 
-        $app->bind('SilexStarter\Migration\Migrator', 'migrator');
+        $app['migration.repository'] = $app->share(
+            function (Application $app) {
+                return new MigrationRepository($app['storage.json'], $app['filesystem']);
+            }
+        );
+        $app['migrator'] = $app->share(
+            function (Application $app) {
+                return new Migrator($app['migration.repository'], $app['module']);
+            }
+        );
+
+        if ($app instanceof SilexStarter) {
+            $app->bind('SilexStarter\Migration\Migrator', 'migrator');
+        }
     }
 
     public function boot(Application $app)
